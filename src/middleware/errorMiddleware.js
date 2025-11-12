b9c7218aaf88730b414c7223f1d191e3cc83bd77
João Martins de Matos Neto <203102@queroquero.local>
2025-11-12 16:17:48 -0300

    refactor errorMiddleware

--- src/middleware/errorMiddleware.js
+++ src/middleware/errorMiddleware.js
@@ -1,17 +1,7 @@
 export const ErrorMiddleware = (err, req, res, next) => {
-    if (err.name === 'ValidationError') {
-        return res.status(err.status).json({ error: err.message });
-    }
-    if (err.name === 'UnauthorizedError') {
-        return res.status(err.status).json({ error: err.message });
-    }
-    if (err.name === 'NotFoundError') {
-        return res.status(err.status).json({ error: err.message });
-    }
-    if (err.name === 'BusinessLogicError') {
-        return res.status(err.status).json({ error: err.message });
-    }
+    const status = err.status || 500;
+    const message = err.message || 'Internal Server Error';
 
     console.error(err);
-    return res.status(500).json({ error: 'Internal Server Error.' });
+    return res.status(status).json({ error: message });
 }
